e8657209fa9248c914ddabf70c126490398248fd
kemaltt <ttnckemal@gmail.com>
2022-04-29 23:14:28 +0200

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const sideNav = document.getElementById('slide-in-nav')
+const mobilNav = document.getElementById('mobil-nav')
+
+
+const showNav = () => {
+
+    mobilNav.style.display = 'none'
+    sideNav.style.visibility = 'visible'
+    sideNav.style.right = 0
+    sideNav.style.left = 0
+}
+const closeNav = () => {
+
+    mobilNav.style.display = 'block'
+    sideNav.style.visibility = 'hidden'
+    sideNav.style.right = '-100%'
+    sideNav.style.left = '100%'
+}
